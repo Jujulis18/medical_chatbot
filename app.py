c16c33e632a193888ba8cce7b3465e0bdd2cf65d
Jujulis18 <140062646+Jujulis18@users.noreply.github.com>
2025-06-05 07:13:37 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import sys
+import os
+
+# Ajouter le répertoire src au path
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+
+if __name__ == "__main__":
+    os.system("streamlit run interface/streamlit_app.py")
