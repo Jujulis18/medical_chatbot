cec07f0f9b2f5009c85d0d52416c7a540c70be13
Jujulis18 <juliethevenin436@gmail.com>
2025-06-16 12:08:10 +0200

    save draft - replace deprecated openAI

--- src/generation/llm_service.py
+++ src/generation/llm_service.py
@@ -1,22 +1,20 @@
-import openai
+from openai import OpenAI
 import numpy as np
 
 class OpenAIChatGenerator:
     def __init__(self, api_key: str, model="gpt-4"):
-        openai.api_key = api_key
+        self.client = OpenAI(api_key=api_key)
         self.model = model
-
+    
     def generate(self, question: str, context_chunks: list[str]) -> str:
         context = "\n\n".join(context_chunks)
         prompt = f"""Tu es un assistant médical. Ne réponds que selon ce contexte.
-
 Contexte :
 {context}
-
 Question :
 {question}
 """
-        response = openai.ChatCompletion.create(
+        response = self.client.chat.completions.create(
             model=self.model,
             messages=[
                 {"role": "system", "content": "Tu es un assistant médical neutre et prudent."},
